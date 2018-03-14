d86f74e143c406cf4d79360bf03b62886eb30fd6
Anup Nayak <anup.nayak@gmail.com>
2018-03-14 10:21:53 -0500

    Solution for #128

---
+++ Array/LongestConsecutiveSequence#128.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Note that the logic is not O(n) complexity as expected in the problem
+// statement. The Result actually beat the 99.2% of submissions.
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+       if(nums.empty()) return 0;
+
+       sort(nums.begin(), nums.end()); 
+       nums.erase( unique( nums.begin(), nums.end() ), nums.end() );
+
+       int max = 1;
+       for (int i = 1; i < nums.size(); i++){
+         if ((nums[i] - nums[i-max]) == max)
+           max++;
+       }
+       return max;
+    }
+};
+int main() {
+  vector<int> data {-1, 100, 4, 4, 200, 1, 3, 2};
+
+  Solution solver;
+  auto ret = solver.longestConsecutive(data);
+  cout << "Longest Consecutive Sequence is " << ret << endl;
+  return 0;
+}
